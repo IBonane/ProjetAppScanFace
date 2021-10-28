b9df69bbfa686339b16441565cb6a968fa01b4db
Ibonane <bonanedjimbala@gmail.com>
2021-10-28 04:24:23 +0200

    AppScanFace

---
+++ DLFaceScan/DLFaceScan/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DLFaceScan
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void StartButtonClicked(object sender, EventArgs e)
+        {
+            StartButtonClickedAsync();
+        }
+
+
+        private async Task StartButtonClickedAsync()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("Erreur", ": La caméra n'est pas disponible !", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+            {
+                await DisplayAlert("Erreur", ": Pas de fichier !", "OK");
+                return;
+            }
+
+
+            // await DisplayAlert("Réuissi", file.Path, "OK");
+
+            await Navigation.PushAsync(new ScannerPage(file)); 
+        }
+
+    }
+}
---
+++ DLFaceScan/DLFaceScan/ScannerPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DLFaceScan
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScannerPage : ContentPage
+    {
+        public ScannerPage(MediaFile file)
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            faceImage.Source = ImageSource.FromStream(() =>
+            {
+                   var stream = file.GetStreamWithImageRotatedForExternalStorage();
+                   return stream;
+            });
+        }
+
+        private void ContinueButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
